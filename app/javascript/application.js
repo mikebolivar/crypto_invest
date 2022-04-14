65a847bf400a3ad727f3b9c11448c822f90f0f5e
Miguel Bolivar <m87.bolivar@gmail.com>
2022-04-14 10:55:10 -0500

    Actualizacion

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,13 +3,6 @@ import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
 
-import jquery from "jquery"
-window.jQuery = jquery
-window.$ = jquery
-
-
-
-$( "#now" ).html('holiiiii')
-window.setInterval(function () {
-  console.log('data', new Date())
-}, 2000);
+import jQuery from "jquery"
+window.jQuery = jQuery
+window.$ = jQuery
